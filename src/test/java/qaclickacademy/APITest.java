e77aa947ab575f282ba726c8b65529bff06a4461
Raed <raid.ismail.s@gmail.com>
2022-02-28 20:03:08 -0800

    Awal commit والحشا

---
+++ src/test/java/qaclickacademy/APITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void postJira() {
+
+		System.out.println("post jira");
+	}
+
+	@Test
+	public void delete() {
+
+		System.out.println("delete");
+
+	}
+
+}
